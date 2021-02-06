642629b5e8b7d52c8a6a11fed93d5fe56831651d
Satwik Nayak <satwikn07@gmail.com>
2021-02-06 11:19:57 +0530

    lo

--- index_script.js
+++ index_script.js
@@ -22,3 +22,24 @@ function cg3(){
 function rc3(){
   document.getElementById("red_change3").style.color=" #3F4450";
 }  
+function createvideo(){
+  var i_frame= document.createElement("ïframe");
+  //  i_frame.setAttribute("id","demo");
+   i_frame.setAttribute("src","https://player.vimeo.com/video/476425296");
+   i_frame.setAttribute("width","640");
+   i_frame.setAttribute("height","360");
+   i_frame.setAttribute("frameborder","0");
+   i_frame.setAttribute("title","vimeo-player");
+   i_frame.setAttribute("allowfullscreen");
+   var video_div=document.getElementById("video_div");
+   video_div.appendChild(i_frame);
+}
+function createvideo1(){
+  var i_frame= document.createElement("ïframe");
+  i_frame.src="https://player.vimeo.com/video/476425296";
+  i_frame.width="640";
+  i_frame.height="360";
+  i_frame.frameborder="0";
+  i_frame.title="vimeo_player";
+  document.getElementById("video_div").appendChild(i_frame);
+}
